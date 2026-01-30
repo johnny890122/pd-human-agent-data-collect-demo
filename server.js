5a30cc3a44b20c8e2917e1984ffcccb7d5c769fe
Xiang Xian, Zhang <j087061439@gmail.com>
2026-01-30 20:26:08 +0800

    fix: fix express path error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Send all requests to index.html to support client-side routing
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
